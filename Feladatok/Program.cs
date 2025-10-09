0d473d39e19d0cc3f20b416dc10d0385cb57f0a0
Bencs Bence Szabolcs <BencsBSz@KKSZKI.LOCAL>
2025-10-09 09:46:23 +0200

    Feladat is ready.

---
+++ Feladatok/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+namespace Feladatok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            DateTime now = DateTime.Now;
+            int ora = now.Hour;
+            if (ora >= 5 && ora < 12)
+            {
+                Console.WriteLine("Jó reggelt!");
+            }
+            else if (ora >= 12 && ora < 18)
+            {
+                Console.WriteLine("Jó napot!");
+            }
+            else
+            {
+                Console.WriteLine("Jó estét!");
+            }
+        }
+    }
+}
