1675eb29dee07667073db4c1adba39f5367c6ee9
Bencs Bence Szabolcs <BencsBSz@KKSZKI.LOCAL>
2025-10-16 08:02:51 +0200

    Sebesseg is ready.

--- Feladatok/Program.cs
+++ Feladatok/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
 
 namespace Feladatok
 {
@@ -34,9 +35,44 @@ namespace Feladatok
             double vissza = osszeg + osszeg * ev * kamat2;
             Console.WriteLine($"A visszajaro osszeg: {vissza}");
             //3
-            //nem tudom :(
-            Console.WriteLine("Adja meg a futó sebességét(m/s):");
-            int sebesseg = int.Parse(Console.ReadLine());
+            double vkezdo;
+            do
+            {
+                Console.WriteLine("Adja meg a futó sebességét 3.00 és 5.00(m/s) között:");
+                vkezdo = double.Parse(Console.ReadLine());
+            }
+            while (vkezdo < 3.0 || vkezdo > 5.0);
+
+            const double tav = 100;
+            double fele = tav / 2;
+            double lassulTav = 10;
+
+            double vMax = vkezdo + 4.0;
+
+            double a1 = (vMax - vkezdo) * (vMax + vkezdo) / (2 * fele);
+
+            Console.WriteLine("Tízméterenként a sebességek (km/h): ");
+
+            for (int i = 10; i <= tav;i=i+10)
+            {
+                double v;
+                if (i <= fele)
+                {
+                    v = vkezdo + (vMax - vkezdo) * (i / fele);
+                }
+                else if (i >= tav - lassulTav)
+                {
+                    double vVeg = vMax / 2;
+                    v = vMax - (vMax - vVeg) * ((i - (tav - lassulTav)) / lassulTav);
+                }
+                else
+                {
+                    v = vMax;    
+                }
+
+                double vKmH = v * 3.6;
+                Console.WriteLine($"{i,3} m: {vKmH,6:F2}");
+            }
         }   
     }
 }
--- Feladatok/obj/Debug/net8.0/Feladatok.AssemblyInfo.cs
+++ Feladatok/obj/Debug/net8.0/Feladatok.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Feladatok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d473d39e19d0cc3f20b416dc10d0385cb57f0a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02091da5726ef022badef2ad48a0d0f73095095b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Feladatok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Feladatok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
